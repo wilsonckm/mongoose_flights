743d29fbc79c0c9057903d8f7a9904efcfeb0801
wilsonckm <wilsonckmchan@gmail.com>
2023-06-20 21:37:45 -0400

    First commit: Completed schema, validation, and default values

---
+++ models/flight.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flighSchema = new Schema ({
+    airline: {type: String, 
+        enum: ['American', 'Southwest', 'United',],
+        required: true,
+    },
+    airport: {type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN',
+        required: true,
+    },
+    flightNo: {type: Number,
+        min: 10,
+        max: 1000,
+        required: true,
+    },
+    departs: {type: Date, 
+        default: function (){
+            const oneYearFromNow = new Date();
+            oneYearFromNow.setFullYear(oneYearFromNow.getFullYear()+1)
+            return oneYearFromNow;
+
+        },
+    },
+});
+
+const Flight = mongoose.model('Flight', flightSchema);
